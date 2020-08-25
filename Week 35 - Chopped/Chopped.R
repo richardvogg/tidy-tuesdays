b0488725b2954bfa372611c14a04fd5eb8f8102f
richardvogg <46679869+richardvogg@users.noreply.github.com>
2020-08-25 10:46:29 -0400

    Changed colors

--- Week 35 - Chopped/Chopped.R
+++ Week 35 - Chopped/Chopped.R
@@ -97,8 +97,8 @@ before+after+plot_annotation(
   title="Fuzzy duplicates",
   subtitle="Replace fuzzy duplicates from the judge names using the stringdist package."
 )&
-  theme(plot.background = element_rect(fill = "moccasin"),
-        panel.background = element_rect(fill="ghostwhite"),
+  theme(plot.background = element_rect(fill = "ivory"),
+        panel.background = element_rect(fill="ivory2"),
         axis.title = element_text(family = "sans" ,size=14,colour=textcol),
         axis.text = element_text(family = "sans" ,size=14,colour=textcol),
         plot.title = element_text(family = "sans", face = "bold", size = 20, colour = textcol),
